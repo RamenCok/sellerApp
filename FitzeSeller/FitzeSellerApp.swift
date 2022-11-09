986e4ad7a5683d072672d8f37bd4736d256ebf55
Bryan Kenneth <bryankenneth0309@gmail.com>
2022-11-09 09:44:29 +0700

    Initial Commit :

---
+++ FitzeSeller/FitzeSellerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitzeSellerApp.swift
+//  FitzeSeller
+//
+//  Created by Bryan Kenneth on 09/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FitzeSellerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
