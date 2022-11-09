da713329e284485ea371bd627362c4cb87bbab70
Stephen Giovanni Saputra <stephen.gsaputra@gmail.com>
2022-11-09 10:39:57 +0700

    ASHU-98 Initial commit

---
+++ FitzeSeller/Notification View/View/NotificationsView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationsView.swift
+//  FitzeSeller
+//
+//  Created by Stephen Giovanni Saputra on 09/11/22.
+//
+
+import SwiftUI
+
+struct NotificationsView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationsView()
+    }
+}
