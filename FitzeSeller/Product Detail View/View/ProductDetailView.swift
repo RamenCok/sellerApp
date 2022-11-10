267c1e00182e8996259d7c3ff9d7cef79f17e7e6
Stephen Giovanni Saputra <stephen.gsaputra@gmail.com>
2022-11-10 12:13:15 +0700

    ASHU-99 created reusable components

--- FitzeSeller/Product Detail View/View/ProductDetailView.swift
+++ FitzeSeller/Product Detail View/View/ProductDetailView.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct ProductDetailView: View {
+    
+    let screen = NSScreen.main?.visibleFrame
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        HStack(spacing: 65) {
+            Image("test")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: 476, maxHeight: 680, alignment: .center)
+                .background(Color.white)
+                .cornerRadius(32)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 32)
+                        .stroke(lineWidth: 1)
+                        .foregroundColor(.gray.opacity(0.15))
+                }
+                .shadow(color: .gray.opacity(0.1), radius: 46, x: 0, y: 4)
+            
+            ProductTextView()
+        }
+        .padding(100)
     }
 }
 
---
+++ FitzeSeller/Product Detail View/View/ProductTextView.swift
@@ -0,0 +1,98 @@
+//
+//  ProductTextView.swift
+//  FitzeSeller
+//
+//  Created by Stephen Giovanni Saputra on 10/11/22.
+//
+
+import SwiftUI
+
+struct ProductTextView: View {
+    
+    var brandName: String = ""
+    var productName: String = ""
+    var colors: [Color] = [.blue, .red, .pink]
+    var productDesc: String = ""
+    
+    var primaryButtonText: String = ""
+    var secondaryButtonText: String = ""
+    
+    var body: some View {
+        
+        VStack(alignment: .leading, spacing: 0) {
+            
+            // MARK: - Product Name and Brand Name
+            Group {
+                Text("Joyce Boutique")
+                    .foregroundColor(.gray.opacity(0.5))
+                    .font(.custom("Sora-Semibold", size: 24))
+                    .padding(.bottom, 10)
+                
+                Text("Zip Through Borg Jacket")
+                    .foregroundColor(.black)
+                    .font(.custom("Sora-Bold", size: 40))
+                    .lineLimit(2)
+                    .padding(.bottom, 45)
+            }
+            
+            // MARK: - Colors
+            HStack(spacing: 16) {
+                ForEach(0..<colors.count) { color in
+                    Circle()
+                        .foregroundColor(colors[color])
+                        .scaledToFit()
+                        .frame(maxWidth: 31, alignment: .center)
+                }
+            }
+            .padding(.bottom, 25)
+            
+            // MARK: - Product Description
+            ScrollView {
+                Text("Dreamy frills to brighten up your day. Crafted from sweat-wicking textured cotton, this mini dress features an A-line silhouette, V-neckline, and flared sleves. Comes with functional side pockets and zipper on the side.\n\nA-line silhouette, V-neckline, and flared sleves. Comes with functional side Dreamy frills to brighten up your day. Crafted from sweat-wicking textured cotton, this mini dress features an A-line silhouette, V-neckline, and flared sleves. Comes with functional side pockets and zipper on the side.\n\nA-line silhouette, V-neckline, and flared sleves. Comes with functional sideDreamy frills to brighten up your day. Crafted from sweat-wicking textured cotton, this mini dress features an A-line silhouette, V-neckline, and flared sleves. Comes with functional side pockets and zipper on the side.\n\nA-line silhouette, V-neckline, and flared sleves. Comes with functional sideDreamy frills to brighten up your day. Crafted from sweat-wicking textured cotton, this mini dress features an A-line silhouette, V-neckline, and flared sleves. Comes with functional side pockets and zipper on the side.\n\nA-line silhouette, V-neckline, and flared sleves. Comes with functional sideDreamy frills to brighten up your day. Crafted from sweat-wicking textured cotton, this mini dress features an A-line silhouette, V-neckline, and flared sleves. Comes with functional side pockets and zipper on the side.\n\nA-line silhouette, V-neckline, and flared sleves. Comes with functional sideDreamy frills to brighten up your day. Crafted from sweat-wicking textured cotton, this mini dress features an A-line silhouette, V-neckline, and flared sleves. Comes with functional side pockets and zipper on the side.\n\nA-line silhouette, V-neckline, and flared sleves. Comes with functional sideDreamy frills to brighten up your day. Crafted from sweat-wicking textured cotton, this mini dress features an A-line silhouette, V-neckline, and flared sleves. Comes with functional side pockets and zipper on the side.\n\nA-line silhouette, V-neckline, and flared sleves. Comes with functional side")
+                    .font(.custom("Sora-Regular", size: 16))
+                    .foregroundColor(.gray)
+            }
+            .frame(maxHeight: 268, alignment: .center)
+            .padding(.bottom, 47)
+            
+            // MARK: - Buttons
+            HStack(spacing: 20) {
+                
+                Button {
+                    print("COK")
+                } label: {
+                    Text("Edit Product")
+                        .font(.custom("Sora-Medium", size: 16))
+                        .padding(.vertical, 16)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.white.opacity(0.0000001))
+                }
+                .foregroundColor(.blue)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 16)
+                        .stroke(lineWidth: 1)
+                        .foregroundColor(.blue)
+                }
+                
+                Button {
+                    
+                } label: {
+                    Text("Archive")
+                        .font(.custom("Sora-Medium", size: 16))
+                        .padding(.vertical, 16)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                }
+                .cornerRadius(16)
+            }
+            .buttonStyle(.plain)
+        }
+    }
+}
+
+struct ProductTextView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductTextView()
+    }
+}
